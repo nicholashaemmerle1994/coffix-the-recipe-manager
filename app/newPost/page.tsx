9d8b9ec59ff1dce6a3b96dd36c252459504e3ee0
nicholashaemmerle1994 <nicholashaemmerle@gmx.at>
2023-03-21 17:23:34 +0100

    Try to debug newpost page

--- app/newPost/page.tsx
+++ app/newPost/page.tsx
@@ -1,25 +1,25 @@
-import { cookies } from 'next/headers';
-import Image from 'next/image';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import { getCategory } from '../../database/category';
-import { getValidSessionByToken } from '../../database/sessions';
+// import { cookies } from 'next/headers';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { redirect } from 'next/navigation';
+// import { getCategory } from '../../database/category';
+// import { getValidSessionByToken } from '../../database/sessions';
 
-export default async function CategoryPage() {
-  // check if there is a valid session
-  const sessionTokenCookie = cookies().get('sessionToken');
-  const session =
-    sessionTokenCookie &&
-    (await getValidSessionByToken(sessionTokenCookie.value));
-  // if there is, redirect to home page
-  if (!session) {
-    redirect('/');
-  }
-  // if not, render login form
-  const category = await getCategory();
+export default function CategoryPage() {
+  // // check if there is a valid session
+  // const sessionTokenCookie = cookies().get('sessionToken');
+  // const session =
+  //   sessionTokenCookie &&
+  //   (await getValidSessionByToken(sessionTokenCookie.value));
+  // // if there is, redirect to home page
+  // if (!session) {
+  //   redirect('/');
+  // }
+  // // if not, render login form
+  // const category = await getCategory();
   return (
     <div className="w-auto h-full max-w-lg mx-auto lg:mt-12 sm:mb-20 sm:mt-12 md:mb-20 bg-default rounded-lg  justify-center align-center flex-col flex text-center ">
-      <h2 className="text-4xl my-12 font-black">Choose your brew method</h2>
+      {/* <h2 className="text-4xl my-12 font-black">Choose your brew method</h2>
       <div className="flex flex-wrap justify-center align-center text-gray-50">
         {category.map((brew) => {
           return (
@@ -41,16 +41,8 @@ export default async function CategoryPage() {
             </Link>
           );
         })}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          width="700pt"
-          height="700pt"
-          viewBox="0 0 700 700"
-        >
-          {' '}
-        </svg>
-      </div>
+      </div> */}
+      Hello
     </div>
   );
 }
