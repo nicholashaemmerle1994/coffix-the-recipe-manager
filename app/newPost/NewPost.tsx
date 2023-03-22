6f286b855646d0d1c77fbc1d90d5cb273b9e17b6
nicholashaemmerle1994 <nicholashaemmerle@gmx.at>
2023-03-22 14:04:00 +0100

    Add newpost page again

--- app/newPost/NewPost.tsx
+++
@@ -1,3 +0,0 @@
-export default function Hello(props: { catergory: any }) {
-  return <div>Hello {props.catergory}</div>;
-}
--- app/newPost/page.tsx
+++ app/newPost/page.tsx
@@ -1,26 +1,25 @@
-// import { cookies } from 'next/headers';
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import { redirect } from 'next/navigation';
-// import { getCategory } from '../../database/category';
-// import { getValidSessionByToken } from '../../database/sessions';
+import { cookies } from 'next/headers';
+import Image from 'next/image';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { getCategory } from '../../database/category';
+import { getValidSessionByToken } from '../../database/sessions';
 
-export default function CategoryPage() {
+export default async function CategoryPage() {
   // // check if there is a valid session
-  // const sessionTokenCookie = cookies().get('sessionToken');
-  // const session =
-  //   sessionTokenCookie &&
-  //   (await getValidSessionByToken(sessionTokenCookie.value));
-  // // if there is, redirect to home page
-  // if (!session) {
-  //   redirect('/');
-  // }
-  // // if not, render login form
-  // const category = await getCategory();
-  // console.log(category);
+  const sessionTokenCookie = cookies().get('sessionToken');
+  const session =
+    sessionTokenCookie &&
+    (await getValidSessionByToken(sessionTokenCookie.value));
+  // if there is, redirect to home page
+  if (!session) {
+    redirect('/');
+  }
+  // if not, render login form
+  const category = await getCategory();
   return (
     <div className="w-auto h-full max-w-lg mx-auto lg:mt-12 sm:mb-20 sm:mt-12 md:mb-20 bg-default rounded-lg  justify-center align-center flex-col flex text-center ">
-      {/* <h2 className="text-4xl my-12 font-black">Choose your brew method</h2>
+      <h2 className="text-4xl my-12 font-black">Choose your brew method</h2>
       <div className="flex flex-wrap justify-center align-center text-gray-50">
         {category.map((brew) => {
           return (
@@ -45,8 +44,7 @@ export default function CategoryPage() {
             </Link>
           );
         })}
-      </div> */}
-      Hello
+      </div>
     </div>
   );
 }
